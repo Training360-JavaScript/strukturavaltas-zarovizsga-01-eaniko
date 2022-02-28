7981c47ea394ae32548afc92df37969e317bd56b
eaniko <69327188+eaniko@users.noreply.github.com>
2022-02-28 16:33:30 +0100

    1-DOM feladat

--- 1-DOM/dom.js
+++ 1-DOM/dom.js
@@ -1,5 +1,10 @@
 function manipulateDom() {
-    // 
-};
+    const elements = document.querySelectorAll('.quote');
+    elements.forEach((e) => {
+        e.style.border = 'blue';
+        e.style.backgroundColor = 'lightblue';
+        e.style.fontStyle = 'italic';
+    });
+}
 
 export { manipulateDom };
